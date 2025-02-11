ed21367495fb0a533f8f4a90c23928047148d206
Sumit Dongare <sumitdongare809@gmail.com>
2025-02-11 16:16:42 +0530

    Final Commit

---
+++ app/src/main/java/com/example/bookmyshow/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.bookmyshow
+
+import android.os.Bundle
+import android.view.Menu
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+
+
+        }
+        supportActionBar?.hide()
+        val toolbar: Toolbar = findViewById(R.id.TooL)
+        setSupportActionBar(toolbar)
+
+        val toolbarImage: ImageView = findViewById(R.id.AppIcon)
+        toolbarImage.setImageResource(R.drawable.image1)
+
+    }
+
+
+
+}
