485ea0ba7a819f311893f8b7536d208e0bb0ff5f
Ashutosh Dishagat <dishagatashutosh@gmail.com>
2025-02-14 13:18:24 +0530

    circle icon

--- app/src/main/java/com/example/bookmyshow/MainActivity.kt
+++ app/src/main/java/com/example/bookmyshow/MainActivity.kt
@@ -1,35 +1,55 @@
 package com.example.bookmyshow
 
 import android.os.Bundle
-import android.view.Menu
-import android.widget.ImageView
-import androidx.appcompat.widget.Toolbar
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.ActionBar
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
+import androidx.viewpager2.widget.ViewPager2
+import com.example.bookmyshow.adapters.BannerAdapter
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewPager: ViewPager2
+    private lateinit var bannerAdapter: BannerAdapter
+    private val handler = Handler(Looper.getMainLooper())
+    private var slideRunnable: Runnable? = null
+
+    // Sample banners (Make sure these images exist in res/drawable)
+    private val bannerImages = listOf(
+        R.drawable.banner1,
+        R.drawable.banner2,
+        R.drawable.banner3,
+        R.drawable.banner4
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
 
+        viewPager = findViewById(R.id.bannerViewPager)
+        bannerAdapter = BannerAdapter(bannerImages)
+        viewPager.adapter = bannerAdapter
 
+        // Enable infinite scrolling effect
+        viewPager.offscreenPageLimit = 1
+        viewPager.currentItem = 0
 
-        }
-
-
+        // Start auto-slide
+        startAutoSlide()
     }
 
+    private fun startAutoSlide() {
+        slideRunnable = object : Runnable {
+            override fun run() {
+                val nextItem = (viewPager.currentItem + 1) % bannerImages.size
+                viewPager.setCurrentItem(nextItem, true)
+                handler.postDelayed(this, 3000) // Slide every 3 seconds
+            }
+        }
+        handler.postDelayed(slideRunnable!!, 3000)
+    }
 
-
+    override fun onDestroy() {
+        super.onDestroy()
+        slideRunnable?.let { handler.removeCallbacks(it) } // Remove callbacks properly
+    }
 }
